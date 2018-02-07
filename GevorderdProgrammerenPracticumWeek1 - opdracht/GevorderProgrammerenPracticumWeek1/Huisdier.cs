fd4c7056373802b3d76938d5fa8ecdb5ba3554c6
Robin-Hogerhuis <32259932+Robin-Hogerhuis@users.noreply.github.com>
2018-02-07 22:34:12 +0100

    Huiswerk1

---
+++ GevorderdProgrammerenPracticumWeek1 - opdracht/GevorderProgrammerenPracticumWeek1/Huisdier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GevorderProgrammerenPracticumWeek1
+{
+    class Huisdier
+    {
+        private string naam;
+        private string soort;
+
+        public Huisdier(string naam, string soort)
+        {
+            this.naam = naam;
+            this.soort = soort;
+        }
+
+        public string getNaam()
+        {
+            return this.naam;
+        }
+
+        public string getSoort()
+        {
+            return this.soort;
+        }
+
+        public string getBeschrijving()
+        {
+            return this.naam + this.soort;  
+        }
+    }
+}
